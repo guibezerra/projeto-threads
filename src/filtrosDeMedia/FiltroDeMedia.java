015a7c2adb962a7700b65418de28d1338ee5aa0c
MuriloHD <muriloddd1@gmail.com>
2022-05-09 20:03:24 -0300

    Mudanças no codigo

--- src/filtrosDeMedia/FiltroDeMedia.java
+++ src/filtrosDeMedia/FiltroDeMedia.java
@@ -1,18 +1,42 @@
 package filtrosDeMedia;
 
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FiltroDeMedia implements Runnable {
 	
-	public void convolucao (int n, int M, int N) {
-		int x1 = n/2;
-		int y1 = n/2;
-		int soma;
-		
-		int w[][] = new int [n][n];
-		int F[][] = new int [M][N];
-		int G[][] = new int [M][N];
-		
-		for (int x = 1; x < M-2; x++) {
-			for (int y = 1; y < N-2; y++) {
+	int w[][];
+	int F[][];
+	int G[][];
+	int x1;
+	int y1;
+	int soma;
+	int M;
+	int N;
+	int n;
+	String tipo;
+	String paleta;
+	
+	public  FiltroDeMedia(int n) {
+		this.n = n;
+		 w = new int [n][n];
+		try {
+			leitura();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (int x = 1; x < N-2; x++) {
+			for (int y = 1; y < M-2; y++) {
 				soma = 0;
 				for (int i = -x1; i < x1; x1++) {
 					for (int j = -y1; j < y1; y1++) {
@@ -23,11 +47,81 @@ public class FiltroDeMedia implements Runnable {
 				G[x][y] = soma;
 			}
 		}
+		try {
+			escrever();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	
+	public void leitura() throws IOException {
+		FileReader fr = new FileReader("C:\\Users\\Murilo\\Downloads\\glassware_noisy.ascii.pgm");
+		BufferedReader br = new BufferedReader(fr);
 		
+		tipo = br.readLine();
+		
+		String tamanho = br.readLine();
+		
+		String[] tamanhoVetor = tamanho.split(" ");
+		
+		paleta = br.readLine();
+		
+		N = ConversorParaInteiro(tamanhoVetor[1]); //Linhas
+		M = ConversorParaInteiro(tamanhoVetor[0]); //colunas
+		
+		F = new int [N][M];
+		G = new int [N][M];
+		
+		String matriz;
+		int i = 0;
+		int j = 0;
+		
+		while((matriz=br.readLine()) != null) {
+			String[] valores = matriz.split(" ");
+			for(int cont = 0; cont < valores.length;cont++) {
+				F[i][j] = ConversorParaInteiro(valores[cont]);
+				j++;
+				if(j > M-1) {
+					j=0;
+					i++;
+				}
+			}
+		}
+		br.close();
+	}
+	
+	public static int ConversorParaInteiro(String S) {
+		return Integer.parseInt(S);
+	}
+	
+	public void escrever() throws IOException {
+		FileWriter fw = new FileWriter("C:\\Users\\Murilo\\Downloads\\Filtro" + Integer.toString(n));
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		bw.write(tipo);
+		bw.newLine();
+		bw.write(Integer.toString(M) + " " + Integer.toString(N));
+		bw.newLine();
+		bw.write(paleta);
+		bw.newLine();
+		
+		int cont = 0;
+		
+		for(int i = 0;i< N ;i++) {
+			for(int j = 0;j< M ;j++) {
+				bw.write(Integer.toString(G[i][j]));
+				cont++;
+				if(cont > 20) {
+					cont = 0;
+					bw.newLine();
+				}
+				else {
+					bw.write(" ");
+				}
+			}
+		}
+		bw.flush();
+		bw.close();
 	}
 }
--- src/processamento/ProcessamentoDeImagens.java
+++ src/processamento/ProcessamentoDeImagens.java
@@ -4,16 +4,54 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import filtrosDeMedia.FiltroDeMedia;
 
 public class ProcessamentoDeImagens {
 	public static void main(String[] args) throws IOException {
 		// leitura de arquivo 
-		FileReader fr = new FileReader("C:\\Users\\guiaa\\Documents\\semestre 2022.1\\sistemas operacionais\\workspace-so\\projeto-threads\\utils\\glassware_noisy.ascii.pgm");
-		BufferedReader br = new BufferedReader(fr);
+		/*
+		 * FileReader fr = new
+		 * FileReader("C:\\Users\\Murilo\\Downloads\\glassware_noisy.ascii.pgm");
+		 * BufferedReader br = new BufferedReader(fr);
+		 * 
+		 * String tipo = br.readLine();
+		 * 
+		 * String tamanho = br.readLine();
+		 * 
+		 * String[] tamanhoVetor = tamanho.split(" ");
+		 * 
+		 * String Paleta = br.readLine();
+		 * 
+		 * int linhas = ConversorParaInteiro(tamanhoVetor[1]); int colunas =
+		 * ConversorParaInteiro(tamanhoVetor[0]);
+		 * 
+		 * String matriz; int[][] imagem = new int[linhas][colunas]; int i = 0; int j =
+		 * 0;
+		 * 
+		 * while((matriz=br.readLine()) != null) { String[] valores = matriz.split(" ");
+		 * 
+		 * 
+		 * for(int cont = 0; cont < valores.length;cont++) { imagem[i][j] =
+		 * ConversorParaInteiro(valores[cont]); j++; if(j > colunas-1) { j=0; i++; } } }
+		 * System.out.println(imagem[427][319]);
+		 */
 		
-		Stream<String> leitura = null; 
-		leitura = br.lines();
-		
-		
+		Thread filtragem3 = new Thread(new FiltroDeMedia(3));
+		Thread filtragem5 = new Thread(new FiltroDeMedia(5));
+		Thread filtragem7 = new Thread(new FiltroDeMedia(7));
+		filtragem3.start();
+		filtragem5.start();
+		filtragem7.start();
 	}
+	
+	
+	
+	public static int ConversorParaInteiro(String S) {
+		return Integer.parseInt(S);
+	}
+	
+	
 }
